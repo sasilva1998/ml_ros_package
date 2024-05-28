73742368d35e17bff940065f2b665a30b199493f
sasilva1998 <sasilva1998@gmail.com>
2024-05-28 17:25:48 +0100

    feat: compatible with ros

--- ml_pipeline_package/src/ml_networks/HM_FCNv2.py
+++ ml_pipeline_package/src/ml_networks/HM_FCNv2.py
@@ -13,13 +13,20 @@ import yaml
 import torchmetrics
 import time
 
+import rospkg
+
+rospack = rospkg.RosPack()
+
+pkg_path = rospack.get_path("ml_pipeline_package")
+
 # Load configuration
+
 try:
-  with open("config/config_HM_FCNv2.yaml", "r") as f:
-    config = yaml.safe_load(f)
+    with open(pkg_path + "/config/config_HM_FCNv2.yaml", "r") as f:
+        config = yaml.safe_load(f)
 except FileNotFoundError:
-  print("Error: Configuration file 'config.yaml' not found!")
-  # Handle the error or use default values
+    print("Error: Configuration file 'config.yaml' not found!")
+    # Handle the error or use default values
 
 file_path_input = config["file_path_input"]
 file_path_output = config["file_path_output"]
@@ -38,47 +45,57 @@ betas = config["betas"]
 alpha = config["alpha"]
 rho = config["rho"]
 
+
 class SocialHeatMapFCN(nn.Module):
     def __init__(self):
         super(SocialHeatMapFCN, self).__init__()
-        
+
         # Encoder (feature extraction)
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1), 
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),  # Add another convolutional layer
+            nn.Conv2d(
+                in_channels=64, out_channels=128, kernel_size=3, padding=1
+            ),  # Add another convolutional layer
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        
+
         # Decoder (upsampling)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(
+                in_channels=128, out_channels=128, kernel_size=4, stride=2, padding=1
+            ),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1),  # Reduce to 32 filters before final layer
+            nn.Conv2d(
+                in_channels=64, out_channels=32, kernel_size=3, padding=1
+            ),  # Reduce to 32 filters before final layer
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(
+                in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1
+            ),
         )
 
         self.softmax = nn.Softmax(dim=1)
-        
+
     def forward(self, x):
         # Forward pass through the encoder
         x = self.encoder(x)
-        
+
         # Forward pass through the decoder
         x = self.decoder(x)
 
         x = self.softmax(x)
-        
+
         return x
-    
+
+
 class HMDataset(Dataset):
     def __init__(self, transform=None) -> None:
         self.data = []
@@ -87,7 +104,7 @@ class HMDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, index):
         sample = self.data[index]
         label = self.labels[index]
@@ -100,18 +117,18 @@ class HMDataset(Dataset):
         label3Channels = one_hot_encode(label.squeeze(0))
 
         return sample, label3Channels
-    
-    def addData(self,data,labels,row_index,column_index):
+
+    def addData(self, data, labels, row_index, column_index):
         data = np.array(data)
         labels = np.array(labels)
 
         data = data.astype(float)
         labels = labels.astype(int)
 
-        reshapeData = data.reshape(row_index,column_index)
+        reshapeData = data.reshape(row_index, column_index)
         dataTensor = torch.tensor(reshapeData)
 
-        reshapeLabels = labels.reshape(row_index,column_index)
+        reshapeLabels = labels.reshape(row_index, column_index)
         labelsTensor = torch.Tensor(reshapeLabels)
 
         self.data.append(dataTensor)
@@ -119,9 +136,10 @@ class HMDataset(Dataset):
 
     def getData(self):
         return self.data
-    
+
     def getLabels(self):
         return self.labels
+
 
 def socialMapToLabels(socialGridMap):
     low_bound = 0.01
@@ -140,24 +158,25 @@ def socialMapToLabels(socialGridMap):
 
     return socialGridMap
 
+
 def one_hot_encode(data_tensor):
-  if data_tensor.dim() != 2:
-    raise ValueError("Input tensor should have 2 dimensions (height and width).")
-  
-  num_channels = 3
+    if data_tensor.dim() != 2:
+        raise ValueError("Input tensor should have 2 dimensions (height and width).")
 
-  one_hot = torch.zeros((num_channels, data_tensor.shape[0], data_tensor.shape[1]))
+    num_channels = 3
 
-  one_hot[0] = torch.where(data_tensor == 0, 1.0, 0.0)  # Channel 0: Low activity
-  one_hot[1] = torch.where(data_tensor == 1, 1.0, 0.0)  # Channel 1: Medium activity
-  one_hot[2] = torch.where(data_tensor == 2, 1.0, 0.0)  # Channel 2: High activity
+    one_hot = torch.zeros((num_channels, data_tensor.shape[0], data_tensor.shape[1]))
 
-  return one_hot
+    one_hot[0] = torch.where(data_tensor == 0, 1.0, 0.0)  # Channel 0: Low activity
+    one_hot[1] = torch.where(data_tensor == 1, 1.0, 0.0)  # Channel 1: Medium activity
+    one_hot[2] = torch.where(data_tensor == 2, 1.0, 0.0)  # Channel 2: High activity
 
-def loadFromTxt(sgmFilename,ogmFilename):
+    return one_hot
+
+
+def loadFromTxt(sgmFilename, ogmFilename):
     pairs = []
-    with open(sgmFilename,"r") as sgmFile, open(ogmFilename,"r") as ogmFile:
-
+    with open(sgmFilename, "r") as sgmFile, open(ogmFilename, "r") as ogmFile:
         sgmReader = csv.reader(sgmFile)
         ogmReader = csv.reader(ogmFile)
 
@@ -166,12 +185,13 @@ def loadFromTxt(sgmFilename,ogmFilename):
         for line1 in sgmReader:
             for line2 in ogmLines:
                 if line1[1] == line2[1]:
-                    pairs.append((line1,line2))
-                    break     
-    
+                    pairs.append((line1, line2))
+                    break
+
     return pairs
 
-def loadIntoDataset(pairs,dataset):
+
+def loadIntoDataset(pairs, dataset):
     for pair in pairs:
         sgm = pair[0]
         ogm = pair[1]
@@ -184,21 +204,27 @@ def loadIntoDataset(pairs,dataset):
         data = ogm[4:]
         labels = socialMapToLabels(sgm[4:])
 
-        dataset.addData(data,labels,row_index,column_index)
+        dataset.addData(data, labels, row_index, column_index)
+
 
 def show_array_as_image(array):
-  plt.imshow(array, cmap='gray')
-  plt.colorbar()
-  plt.show()
+    plt.imshow(array, cmap="gray")
+    plt.colorbar()
+    plt.show()
+
 
 def pad_array_to_shape(array, target_shape, pad_value=0):
-        # Calculate the padding amounts for each dimension
-    pad_width = [(target_shape[0] - array.shape[0], 0), (target_shape[1] - array.shape[1], 0)]
+    # Calculate the padding amounts for each dimension
+    pad_width = [
+        (target_shape[0] - array.shape[0], 0),
+        (target_shape[1] - array.shape[1], 0),
+    ]
 
     # Pad the array using np.pad
-    padded_array = np.pad(array, pad_width, mode='constant', constant_values=pad_value)
+    padded_array = np.pad(array, pad_width, mode="constant", constant_values=pad_value)
 
     return padded_array
+
 
 def find_matching_files(folder_path):
     matching_files = {}
@@ -213,31 +239,37 @@ def find_matching_files(folder_path):
                 matching_files[file_number][map_type] = os.path.join(folder_path, file)
     return matching_files
 
-def addFilesToDataset(matching_files,dataset):
+
+def addFilesToDataset(matching_files, dataset):
     for file_number, files in matching_files.items():
-        if 'socialGridMap' in files and 'obstacleGridMap' in files:
-            sgmFilename = files['socialGridMap']
-            ogmFilename = files['obstacleGridMap']
+        if "socialGridMap" in files and "obstacleGridMap" in files:
+            sgmFilename = files["socialGridMap"]
+            ogmFilename = files["obstacleGridMap"]
             pairs = loadFromTxt(sgmFilename, ogmFilename)
             print(f"Pairs for files with number {file_number}:")
-            loadIntoDataset(pairs,dataset)
+            loadIntoDataset(pairs, dataset)
+
 
 if __name__ == "__main__":
-
     # Define transformations
-    transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((training_image_size[0], training_image_size[1]), interpolation=transforms.InterpolationMode.NEAREST),
-    transforms.ToTensor()
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.ToPILImage(),
+            transforms.Resize(
+                (training_image_size[0], training_image_size[1]),
+                interpolation=transforms.InterpolationMode.NEAREST,
+            ),
+            transforms.ToTensor(),
+        ]
+    )
 
-    model = SocialHeatMapFCN() # Instantiate the model
+    model = SocialHeatMapFCN()  # Instantiate the model
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = SocialHeatMapFCN().to(device)
 
-    accuracy = torchmetrics.Accuracy(task="multiclass",num_classes=num_classes)
+    accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes)
     accuracy = accuracy.to(device)
 
     if criterion == 1:
@@ -262,51 +294,60 @@ if __name__ == "__main__":
 
     matchingFiles = find_matching_files(file_path_input)
 
-    tolerance = 0.0000000000000000001  # Threshold for loss function change (adjust as needed)
-    prev_loss = float('inf')  # Initialize with a high value
+    tolerance = (
+        0.0000000000000000001  # Threshold for loss function change (adjust as needed)
+    )
+    prev_loss = float("inf")  # Initialize with a high value
     plateau_tolerance = 25
 
     for file_number, files in matchingFiles.items():
         plateau_count = 0
-        if 'socialGridMap' in files and 'obstacleGridMap' in files:
+        if "socialGridMap" in files and "obstacleGridMap" in files:
             newDataset = HMDataset(transform=transform)
-            sgmFilename = files['socialGridMap']
-            ogmFilename = files['obstacleGridMap']
-            pairs = loadFromTxt(sgmFilename, ogmFilename,)
+            sgmFilename = files["socialGridMap"]
+            ogmFilename = files["obstacleGridMap"]
+            pairs = loadFromTxt(
+                sgmFilename,
+                ogmFilename,
+            )
             print(f"file number: {file_number}")
-            loadIntoDataset(pairs,newDataset)
+            loadIntoDataset(pairs, newDataset)
 
-            newDataLoader = DataLoader(newDataset,batch_size=batch_size,shuffle=True)
+            newDataLoader = DataLoader(newDataset, batch_size=batch_size, shuffle=True)
 
             stop = False
 
             for epoch in range(num_epochs):
-            # Iterate over the dataset
+                # Iterate over the dataset
                 if stop != True:
                     for inputs, labels in newDataLoader:
                         inputs = inputs.to(device)
                         labels = labels.to(device)
-                    # Forward pass
+                        # Forward pass
                         outputs = model(inputs)
-                    # Compute the loss
+                        # Compute the loss
                         loss = criterion(outputs, labels)
                         print(loss.item())
 
                         if abs(prev_loss - loss.item()) > tolerance:
                             prev_loss = loss.item()  # Update previous loss
-                            plateau_count = 0  # Reset plateau counter if improvement detected
+                            plateau_count = (
+                                0  # Reset plateau counter if improvement detected
+                            )
                         else:
                             plateau_count += 1  # Increment counter if loss plateaus
                         # Move to next file if plateau_tolerance is reached
                         if plateau_count >= plateau_tolerance:
-                            print(f"Loss plateaued for {plateau_tolerance} epochs. Moving to next file.")
+                            print(
+                                f"Loss plateaued for {plateau_tolerance} epochs. Moving to next file."
+                            )
                             stop = True
-                    # Backward pass and optimization
+                        # Backward pass and optimization
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
 
-                        accuracy.update(outputs,labels)
+                        accuracy.update(outputs, labels)
 
             del newDataset
             del newDataLoader
--- ml_pipeline_package/src/ml_networks/sub_and_pub.py
+++ ml_pipeline_package/src/ml_networks/sub_and_pub.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import rospy
 from grid_map_msgs.msg import GridMap, GridMapInfo
 from nav_msgs.msg import OccupancyGrid
@@ -11,54 +13,62 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import cv2
+import rospkg
+from scipy.ndimage.interpolation import map_coordinates
 
+
+rospack = rospkg.RosPack()
+
+pkg_path = rospack.get_path("ml_pipeline_package")
 
 # Load configuration
 try:
-  with open("config/config_record_maps.yaml", "r") as f:
-    config = yaml.safe_load(f)
+    with open(pkg_path + "/config/config_record_maps.yaml", "r") as f:
+        config = yaml.safe_load(f)
 except FileNotFoundError:
-  print("Error: Configuration file 'config.yaml' not found!")
-  # Handle the error or use default values
+    print("Error: Configuration file 'config.yaml' not found!")
+    # Handle the error or use default values
 
 folder_path = config["folder_path"]
 ogm_topic = config["ogm_topic"]
 sgm_topic = config["sgm_topic"]
 
 model = SocialHeatMapFCN()
-model_path = "data/trained_models/office/FCNv2MODEL.pt"
+model_path = pkg_path + "/data/trained_models/office/FCNv2MODEL.pt"
 
 model.load_state_dict(torch.load(model_path))
 
 model.eval()
 
-def o_gridmap_callback(msg,model):
-    '''
-header.seq
-header.stamp.secs
-header.stamp.nsecs
-header.frame_id
-info.map_load_time.secs
-info.map_load_time.nsecs
-info.resolution
-info.width
-info.height
-info.origin.position.x
-info.origin.position.y
-info.origin.position.z
-info.origin.orientation.x
-info.origin.orientation.y
-info.origin.orientation.z
-info.origin.orientation.w
-data
-'''
+
+def o_gridmap_callback(msg, model):
+    """
+    header.seq
+    header.stamp.secs
+    header.stamp.nsecs
+    header.frame_id
+    info.map_load_time.secs
+    info.map_load_time.nsecs
+    info.resolution
+    info.width
+    info.height
+    info.origin.position.x
+    info.origin.position.y
+    info.origin.position.z
+    info.origin.orientation.x
+    info.origin.orientation.y
+    info.origin.orientation.z
+    info.origin.orientation.w
+    data
+    """
     data = msg.data
     column_index = msg.info.width
     row_index = msg.info.height
     seq = msg.header.seq
+    frame_id = msg.header.frame_id
 
-    numpy_array = np.array(data,dtype=np.float32)
-    numpy_array = numpy_array.reshape(row_index,column_index)
+    numpy_array = np.array(data, dtype=np.float32)
+    numpy_array = numpy_array.reshape(row_index, column_index)
     # Resize to 128x128 using OpenCV (adjust interpolation method if needed)
     resized_array = cv2.resize(numpy_array, (128, 128), interpolation=cv2.INTER_AREA)
     image_tensor = torch.from_numpy(resized_array)
@@ -82,42 +92,104 @@ data
     np_low = np_low.astype(int)
 
     np_medium = np.array(np_medium)
-    np_medium= np.floor(np_medium * 100)
+    np_medium = np.floor(np_medium * 100)
     np_medium = np_medium.astype(int)
 
-
     np_high = np.array(np_high)
-    np_high  = np.floor(np_high * 100)
+    np_high = np.floor(np_high * 100)
     np_high = np_high.astype(int)
 
     low_msg = OccupancyGrid()
     medium_msg = OccupancyGrid()
     high_msg = OccupancyGrid()
 
-    low_msg.header.seq = seq
+    # low_msg.header.seq = seq
+    # low_msg.header.frame_id = frame_id
+    low_msg.header = msg.header
     low_msg.info.height = 128
     low_msg.info.width = 128
     low_msg.data = np_low.tolist()
 
-    medium_msg.header.seq = seq
-    medium_msg.info.height = 128
-    medium_msg.info.width = 128
-    medium_msg.data = np_medium.tolist()
+    # medium_msg.header.seq = seq
+    # medium_msg.header.frame_id = frame_id
+    medium_msg.header = msg.header
+    medium_msg.info.map_load_time = msg.info.map_load_time
+    medium_msg.info.resolution = msg.info.resolution
+    medium_msg.info.origin = msg.info.origin
+    medium_msg.info.height = msg.info.height
+    medium_msg.info.width = msg.info.width
 
-    high_msg.header.seq = seq
-    high_msg.info.height = 128
-    high_msg.info.width = 128
-    high_msg.data = np_high.tolist()
+    # ! medium zone processing
+
+    medium_zones = np_medium.reshape(128, 128)
+
+    new_dims = []
+    for original_length, new_length in zip(
+        medium_zones.shape, (msg.info.height, msg.info.width)
+    ):
+        new_dims.append(np.linspace(0, original_length - 1, new_length))
+
+    coords = np.meshgrid(*new_dims, indexing="ij")
+    medium_zones_reshaped = map_coordinates(medium_zones, coords)
+
+    medium_zones_reshaped = medium_zones_reshaped.flatten()
+    medium_zones_reshaped = medium_zones_reshaped.tolist()
+    medium_zones_reshaped = [int(x) for x in medium_zones_reshaped]
+    medium_zones_reshaped = [0 if x < 0 else x for x in medium_zones_reshaped]
+
+    medium_msg.data = medium_zones_reshaped
+
+    # ! #############################
+
+    # high_msg.header.seq = seq
+    # high_msg.header.frame_id = frame_id
+    high_msg.header = msg.header
+    high_msg.info.map_load_time = msg.info.map_load_time
+    high_msg.info.resolution = msg.info.resolution
+    high_msg.info.origin = msg.info.origin
+    high_msg.info.height = msg.info.height
+    high_msg.info.width = msg.info.width
+
+    # ! high zone processing
+
+    high_zones = np_high.reshape(128, 128).astype("float32")
+
+    new_dims = []
+    for original_length, new_length in zip(
+        high_zones.shape, (msg.info.height, msg.info.width)
+    ):
+        new_dims.append(np.linspace(0, original_length - 1, new_length))
+
+    coords = np.meshgrid(*new_dims, indexing="ij")
+    high_zones_reshaped = map_coordinates(high_zones, coords)
+
+    # high_zones_reshaped = cv2.resize(
+    #     high_zones,
+    #     dsize=(msg.info.height, msg.info.width),
+    #     interpolation=cv2.INTER_CUBIC,
+    # )
+
+    high_zones_reshaped = high_zones_reshaped.flatten()
+    high_zones_reshaped = high_zones_reshaped.tolist()
+    high_zones_reshaped = [0 if x < 0 else x for x in high_zones_reshaped]
+    high_zones_reshaped = [int(x) for x in high_zones_reshaped]
+
+    high_msg.data = high_zones_reshaped
+
+    # ! #############################
 
     sgmPubLow.publish(low_msg)
     sgmPubMedium.publish(medium_msg)
     sgmPubHigh.publish(high_msg)
 
-rospy.init_node("mlNetwork")
-ogmSub = rospy.Subscriber(ogm_topic, OccupancyGrid, o_gridmap_callback,callback_args=model)
 
-sgmPubLow = rospy.Publisher("/ml_sgm_low",OccupancyGrid,queue_size=10)
-sgmPubMedium = rospy.Publisher("/ml_sgm_medium",OccupancyGrid,queue_size=10)
-sgmPubHigh = rospy.Publisher("/ml_sgm_high",OccupancyGrid,queue_size=10)
+rospy.init_node("mlNetwork")
+ogmSub = rospy.Subscriber(
+    ogm_topic, OccupancyGrid, o_gridmap_callback, callback_args=model
+)
+
+sgmPubLow = rospy.Publisher("/ml_sgm_low", OccupancyGrid, queue_size=10)
+sgmPubMedium = rospy.Publisher("/ml_sgm_medium", OccupancyGrid, queue_size=10)
+sgmPubHigh = rospy.Publisher("/ml_sgm_high", OccupancyGrid, queue_size=10)
 
 rospy.spin()
